35dbebe408c4aa4395243d175a0adcc9ad68ff72
D-Ono <hiromi.ono@unesp.br>
2021-07-31 23:31:19 -0300

    feat: add Register School

---
+++ doakid/src/pages/CadastroEscola/index.js
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import {Link} from 'react-router-dom';
+
+import Input from '../../components/Input';
+import { Register, RegisterContainer } from './style';
+import { SideLogin } from '../../components/SideLogin'
+import promiseImage from "../../assets/promise.svg";
+import { Button } from 'antd';
+
+function RegisterSchoolPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <Register>
+      <SideLogin>
+        <h1>DoaKid</h1>
+        <h3>Já tem uma conta? <Link to='/'>Entre aqui.</Link></h3>
+        <img src={promiseImage} height="300px" width="300px" alt="Imagem de Solidariedade"></img>
+       </SideLogin>
+      <RegisterContainer>
+        <form>
+          <h1>Cadastrar Escola</h1>
+          <div>
+            <Button><Link to="/CadastroEscola">Escola</Link></Button>
+            <Button><Link to="/Cadastro">Usuarios</Link></Button>
+          </div>
+          <div className="input-escola">
+            <Input
+              name="nome"
+              placeholder="Nome da Escola"
+            />
+            <Input
+              name="codigo"
+              placeholder="Código da Escola"
+            />
+          </div>
+          <div className="input-escola">
+          <Input
+            type="email"
+            name="email"
+            placeholder="E-mail"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            required
+          />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Senha"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            required
+          />
+          </div>
+          <div className="input-escola">
+          <Input
+            name="Rua"
+            placeholder="Rua"
+          />
+          <Input
+            name="Numero"
+            placeholder="Numero"
+          />
+          </div>
+          <div className="input-escola">
+          <Input
+            name="Bairro"
+            placeholder="Bairro"
+          />
+          <Input
+            name="Telefone"
+            placeholder="Telefone"
+          />
+          </div>  
+          <div className="input-escola">
+            <Input
+              name="Horario"
+              placeholder="Escola abre às..."
+            />
+            <Input
+              name="Horario"
+              placeholder="Escola fecha às..."
+            />
+          </div>
+          <button className="buttonSubmit" type="submit">Cadastrar</button>
+        </form>
+      </RegisterContainer>
+    </Register>
+  );
+}
+
+export default RegisterSchoolPage;
---
+++ doakid/src/pages/CadastroEscola/style.js
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+
+export const Register = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const RegisterContainer = styled.div`
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  h1 {
+    padding: 3rem;
+    text-align: center;
+    font-size: 4rem;
+    font-weight: 700;
+    color: var(--color-title-text);
+  }
+  .buttonSubmit {
+    width: 20rem;
+    height: 5.6rem;
+    background: var(--color-button-background);
+    color: var(--color-highlight-text);
+    border: 0;
+    border-radius: 0.6rem;
+    cursor: pointer;
+    font-weight: 700;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-decoration: none;
+    transition: 0.2s;
+    margin: 2rem 0 1.2rem 0;
+  }
+  .buttonSubmit:hover {
+    opacity: 0.8;
+  }
+  form {
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .input-escola{
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+  }
+  @media (min-width: 768px) {
+    max-width: 100vw;
+    padding: 6rem 5rem;
+    margin: 0 8rem;
+    h1 {
+      margin-bottom: 3.6rem;
+    }
+  }
+`;
