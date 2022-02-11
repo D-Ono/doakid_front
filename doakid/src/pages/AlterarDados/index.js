f461fc1099db8aeab1e9e7f14837925ab50866ca
D-Ono <hiromi.ono@unesp.br>
2022-02-11 11:49:22 -0300

    feat: alterando dados

--- doakid/src/pages/AlterarDados/index.js
+++ doakid/src/pages/AlterarDados/index.js
@@ -1,11 +1,85 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BodyContainer, Container, DataContainer, InputDiv, PhotoContainer } from './style';
 import Header from '../../components/Header';
 import userPhoto from '../../assets/user.png'
+import api from "../../services/api";
+import { message, Modal } from 'antd';
+import { ExclamationCircleOutlined } from '@ant-design/icons';
+import { useHistory } from "react-router-dom";
 
 function AlterarDados() {
-  const username = localStorage.getItem('@doakid/username');
-  const email = localStorage.getItem('@doakid/email');
+  const history = useHistory();
+  const { confirm } = Modal;
+  const userCode = localStorage.getItem('@doakid/userCode');
+  const [nome, setNome] = useState('');
+  const [sobrenome, setSobrenome] = useState('');
+  const [email_familia, setEmailFamilia] = useState('');
+  const [numero, setNumero] = useState('');
+  const [senha, setSenha] = useState('');
+  const [senhaAtual, setSenhaAtual] = useState('');
+  const [novaSenha, setNovaSenha] = useState('');
+  const [novaSenhaConfirm, setNovaSenhaConfirma] = useState('');
+  
+  useEffect(() => {
+    api.post(`http://localhost:5000/familia/user/${userCode}`, {cod_familia: userCode}).then(
+      (response) => {
+        setNome(response.data[0].nome);
+        setSobrenome(response.data[0].sobrenome);
+        setEmailFamilia(response.data[0].email_familia);
+        setNumero(response.data[0].numero);
+        setSenha(response.data[0].senha)
+        setSenhaAtual(response.data[0].senha)
+        setNovaSenha(response.data[0].senha)
+        setNovaSenhaConfirma(response.data[0].senha)
+      });
+    }, []);
+    
+    function showDeleteConfirm() {
+      confirm({
+        title: 'Você realmente deseja Alterar seus Dados?',
+        icon: <ExclamationCircleOutlined />,
+        content: 'Certifique se que os dados estão preenchidos corretamente, principlamente a senha.',
+        okText: 'Alterar',
+        okType: 'danger',
+        cancelText: 'Cancelar',
+        async onOk() {
+          if(senha !== senhaAtual || novaSenha !== novaSenhaConfirm){
+            if(senha !== senhaAtual){
+              message.error("A Senha Atual está Incorreta. Os dados não foram alterados.", 3);
+              history.push("/FamilyProfile");
+            }
+            if(novaSenha !== novaSenhaConfirm){
+              message.error("A nova senha está divergente!",2);
+              history.push("/FamilyProfile");
+            } 
+          }
+          else {
+            
+            try {
+              const response = await api.put("http://localhost:5000/familia/update",
+              {
+                nome, 
+                sobrenome, 
+                email_familia, 
+                numero, 
+                novaSenha, 
+                userCode
+              });
+              if(response.status === 200) {
+                message.success('Os dados foram alterados com Sucesso!', 2);
+                history.push("/FamilyProfile");
+              }
+            } catch (error) {
+              console.log(error)
+              console.log(error.response);
+            }
+          }
+        },
+        onCancel() {
+          console.log("Cancelado");
+        },
+      });
+    }
 
   return (
       <Container>
@@ -17,10 +91,20 @@ function AlterarDados() {
           </PhotoContainer>
           <DataContainer>
           <InputDiv>
-            <label>Nome Completo: </label>
+            <label>Primeiro Nome: </label>
             <input
               type="name"
-              placeholder="Nome Completo"
+              value= {nome}
+              onChange={(e) => setNome(e.target.value)}
+              className="input"
+            />
+          </InputDiv>
+          <InputDiv>
+            <label>Sobrenome: </label>
+            <input
+              type="name"
+              value= {sobrenome}
+              onChange={(e) => setSobrenome(e.target.value)}
               className="input"
             />
           </InputDiv>
@@ -28,7 +112,8 @@ function AlterarDados() {
             <label>E-Mail: </label>
             <input
               type="email"
-              placeholder="E-Mail"
+              value={email_familia }
+              onChange={(e) => setEmailFamilia(e.target.value)}
               className="input"
             />
           </InputDiv>
@@ -36,16 +121,19 @@ function AlterarDados() {
             <label>Telefone: </label>
             <input
               type="name"
-              placeholder="Telefone"
+              value={numero}
+              onChange={(e) => setNumero(e.target.value)}
               className="input"
             />
           </InputDiv>
+          <br/>
           <InputDiv>
             <label>Senha atual: </label>
             <input
               type="password"
               placeholder="Senha Atual"
               className="input"
+              onChange={(e) => setSenhaAtual(e.target.value)}
             />
           </InputDiv>
           <InputDiv>
@@ -54,6 +142,7 @@ function AlterarDados() {
               type="password"
               placeholder="Nova Senha"
               className="input"
+              onChange={(e) => setNovaSenha(e.target.value)}
             />
           </InputDiv>
           <InputDiv>
@@ -62,9 +151,10 @@ function AlterarDados() {
               type="password"
               placeholder="Nova Senha"
               className="input"
+              onChange={(e) => setNovaSenhaConfirma(e.target.value)}
             />
           </InputDiv>
-            <button className="changePhoto">Alterar Dados</button>
+            <button className="changePhoto" onClick={showDeleteConfirm} type="dashed">Alterar Dados</button>
           </DataContainer>
         </BodyContainer>
       </Container>
--- doakid/src/pages/AlterarDadosEscola/index.js
+++ doakid/src/pages/AlterarDadosEscola/index.js
@@ -1,11 +1,97 @@
-import React from 'react';
+import React, { useEffect, useState }  from 'react';
 import { BodyContainer, Container, DataContainer, InputDiv, PhotoContainer } from './style';
 import Header from '../../components/Header';
 import userPhoto from '../../assets/user.png'
+import api from "../../services/api";
+import { message, Modal } from 'antd';
+import { ExclamationCircleOutlined } from '@ant-design/icons';
+import { useHistory } from "react-router-dom";
 
-function AlterarDados() {
-  const username = localStorage.getItem('@doakid/username');
-  const email = localStorage.getItem('@doakid/email');
+function AlterarDadosEscola() {
+    const history = useHistory();
+    const { confirm } = Modal;
+    const userCode = localStorage.getItem('@doakid/userCode');
+    const [nome_escola, setNomeEscola] = useState('');
+    const [email_escola, setEmailEscola] = useState('');
+    const [rua_escola, setRuaEscola] = useState('');
+    const [numero_rua, setNumeroRua] = useState(0);
+    const [bairro_escola, setBairroEscola] = useState('');
+    const [horario_funcionamento_inicio, setHorarioAbertura] = useState('');
+    const [horario_funcionamento_fim, setHorarioFechamento ] = useState('');
+    const [horario, setHorario ] = useState('');
+    const [numero, setNumero] = useState('');
+    const [senha, setSenha] = useState('');
+    const [senhaAtual, setSenhaAtual] = useState('');
+    const [novaSenha, setNovaSenha] = useState('');
+    const [novaSenhaConfirm, setNovaSenhaConfirma] = useState('');
+  
+    useEffect(() => {
+        api.post(`http://localhost:5000/escola/user/${userCode}`, {cod_escola: userCode}).then(
+          (response) => {
+            setNomeEscola(response.data[0].nome_escola);
+            setEmailEscola(response.data[0].email_escola);
+            setRuaEscola(response.data[0].rua_escola);
+            setNumeroRua(response.data[0].numero_rua);
+            setBairroEscola(response.data[0].bairro_escola);
+            setHorarioAbertura(response.data[0].horario_funcionamento_inicio);
+            setHorarioFechamento(response.data[0].horario_funcionamento_fim);
+            setNumero(response.data[0].numero);
+            setSenha(response.data[0].senha)
+            setSenhaAtual(response.data[0].senha)
+            setNovaSenha(response.data[0].senha)
+            setNovaSenhaConfirma(response.data[0].senha)
+            setHorario(response.data[0].horario_funcionamento_inicio.substring(0, 5) + ' - ' + response.data[0].horario_funcionamento_fim.substring(0, 5))
+          });
+        }, []);
+        
+        function showDeleteConfirm() {
+          confirm({
+            title: 'Você realmente deseja Alterar seus Dados?',
+            icon: <ExclamationCircleOutlined />,
+            content: 'Certifique se que os dados estão preenchidos corretamente, principlamente a senha.',
+            okText: 'Alterar',
+            okType: 'danger',
+            cancelText: 'Cancelar',
+            async onOk() {
+              if(senha !== senhaAtual || novaSenha !== novaSenhaConfirm){
+                if(senha !== senhaAtual){
+                  message.error("A Senha Atual está Incorreta. Os dados não foram alterados.", 3);
+                  history.push("/SchoolProfile");
+                }
+                if(novaSenha !== novaSenhaConfirm){
+                  message.error("A nova senha está divergente!",2);
+                  history.push("/SchoolProfile");
+                } 
+              }
+              else {
+                try {
+                  const response = await api.put("http://localhost:5000/escola/update",
+                  {
+                    nome_escola, 
+                    email_escola, 
+                    rua_escola,
+                    numero_rua, 
+                    bairro_escola,
+                    horario_funcionamento_inicio,
+                    horario_funcionamento_fim,
+                    userCode, 
+                    numero
+                  });
+                  if(response.status === 200) {
+                    message.success('Os dados foram alterados com Sucesso!', 2);
+                    history.push("/SchoolProfile");
+                  }
+                } catch (error) {
+                  console.log(error)
+                  console.log(error.response);
+                }
+              }
+            },
+            onCancel() {
+              console.log("Cancelado");
+            },
+          });
+        }
 
   return (
       <Container>
@@ -21,6 +107,8 @@ function AlterarDados() {
                 <input
                 type="name"
                 placeholder="Nome da Escola"
+                value= {nome_escola}
+                onChange={(e) => setNomeEscola(e.target.value)}
                 className="input"
                 />
             </InputDiv>
@@ -30,6 +118,8 @@ function AlterarDados() {
                 type="email"
                 placeholder="E-Mail"
                 className="input"
+                value= {email_escola}
+                onChange={(e) => setEmailEscola(e.target.value)}
                 />
             </InputDiv>
             <InputDiv>
@@ -38,6 +128,8 @@ function AlterarDados() {
                 type="name"
                 placeholder="Telefone"
                 className="input"
+                value= {numero}
+                onChange={(e) => setNumero(e.target.value)}
                 />
             </InputDiv>
             <InputDiv>
@@ -46,6 +138,8 @@ function AlterarDados() {
                 type="text"
                 placeholder="Endereço "
                 className="input"
+                value= {rua_escola}
+                onChange={(e) => setRuaEscola(e.target.value)}
                 />
             </InputDiv>
             <InputDiv>
@@ -54,6 +148,8 @@ function AlterarDados() {
                 type="number"
                 placeholder="Número"
                 className="input"
+                value= {numero_rua}
+                onChange={(e) => setNumeroRua(e.target.value)}
                 />
             </InputDiv>
             <InputDiv>
@@ -62,6 +158,8 @@ function AlterarDados() {
                 type="text"
                 placeholder="Bairro"
                 className="input"
+                value= {bairro_escola}
+                onChange={(e) => setBairroEscola(e.target.value)}
                 />
             </InputDiv>
             <InputDiv>
@@ -70,37 +168,42 @@ function AlterarDados() {
                 type="text"
                 placeholder="Horario de Funcionamento"
                 className="input"
+                value= {horario}
+                onChange={(e) => setHorario(e.target.value)}
                 />
             </InputDiv>
             <InputDiv>
-                <label>Senha atual: </label>
-                <input
-                type="password"
-                placeholder="Senha Atual"
-                className="input"
-                />
+            <label>Senha atual: </label>
+            <input
+              type="password"
+              placeholder="Senha Atual"
+              className="input"
+              onChange={(e) => setSenhaAtual(e.target.value)}
+            />
+          </InputDiv>
+          <InputDiv>
+            <label>Nova Senha: </label>
+            <input
+              type="password"
+              placeholder="Nova Senha"
+              className="input"
+              onChange={(e) => setNovaSenha(e.target.value)}
+            />
+          </InputDiv>
+          <InputDiv>
+            <label>Confirme a Nova Senha: </label>
+            <input
+              type="password"
+              placeholder="Nova Senha"
+              className="input"
+              onChange={(e) => setNovaSenhaConfirma(e.target.value)}
+            />
             </InputDiv>
-            <InputDiv>
-                <label>Nova Senha: </label>
-                <input
-                type="password"
-                placeholder="Nova Senha"
-                className="input"
-                />
-            </InputDiv>
-            <InputDiv>
-                <label>Confirme a Nova Senha: </label>
-                <input
-                type="password"
-                placeholder="Nova Senha"
-                className="input"
-                />
-            </InputDiv>
-            <button className="changePhoto">Alterar Dados</button>
+            <button className="changePhoto" onClick={showDeleteConfirm} type="dashed">Alterar Dados</button>
           </DataContainer>
         </BodyContainer>
       </Container>
   );
 }
 
-export default AlterarDados;
+export default AlterarDadosEscola;
